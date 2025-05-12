9b615550c3c8b2751a55a7b12bdb79cbfff401c9
Vasilis Zoidis <billzoidis@gmail.com>
2025-05-12 14:29:10 +0300

    Measuments implemented

--- arduino/measurements/SeaWeatherStation.ino
+++ arduino/measurements/SeaWeatherStation.ino
@@ -27,10 +27,10 @@ void setup() {
   
   Serial.println(F("BMP280 test"));
   
-  //if  (!bmp.begin()) {
-  //Serial.println(F("Could not find a valid BMP280 sensor,  check wiring!"));
-  //while (1);
-  //}
+  if  (!bmp.begin()) {
+  Serial.println(F("Could not find a valid BMP280 sensor,  check wiring!"));
+  while (1);
+  }
 /* Default settings from datasheet.  */
 bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                 Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
@@ -40,7 +40,14 @@ bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
 }
 
 void loop() {
-  /*Serial.print(F("Temperature  = "));
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  data.Z = map(az, -17000, 17000, 0, 255);  // Z axis data
+  
+  delay(100);
+  
+  Serial.print("Axis Z:");
+  Serial.println(az-20000);
+  Serial.print(F("Temperature  = "));
   Serial.print(bmp.readTemperature());
   Serial.println(" *C");
   
@@ -53,25 +60,7 @@ void loop() {
   Serial.println("  m");                    //If you don't know it, modify it until you get your current  altitude
   
   Serial.println();
-  delay(2000);*/
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  data.X = map(ax, -17000, 17000, 0, 255); // X axis data
-  data.Y = map(ay, -17000, 17000, 0, 255);  // Y axis data
-  data.Z = map(az, -17000, 17000, 0, 255);  
-  data.GX = map(gx, -17000, 17000, 0, 360);
-  data.GY = map(gy, -17000, 17000, 0, 360);
-  data.GZ = map(gz, -32768, 32767, 0, 360); // Rotation around Z axis
-  //delay(100);
-  //Serial.print("Axis_X:");
-  //Serial.println(data.X-128);
-  //Serial.print("Axis_Y:");
-  //Serial.println(data.Y);
-  Serial.print("Axis_Z:");
-  Serial.println(az-17340);
-  /*Serial.print("Axis_GX:");
-  Serial.println(data.GX);
-  Serial.print("Axis_GY:");
-  Serial.println(data.GY);
-  Serial.print("Axis_GZ:");
-  Serial.println(data.GZ);*/
+  //delay(900);
+  
+  
 }
